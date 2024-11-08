a58f62d0fbbe2ca1b645edd0cf010eca4871ff0c
Junyu (Tim) Yao <197yjy@gmail.com>
2024-11-08 10:33:58 -0500

    ATM-Updated

--- src/pages/aehl-us/ATM-info.tsx
+++ src/pages/aehl-us/ATM-info.tsx
@@ -22,7 +22,7 @@ const bondType: BondType[] = [
     name: "ATM No. 1",
     investAmount: "50000",
     interest: 12,
-    paid: "Paid by Monthly",
+    paid: "Monthly Paydown Plan",
     investReturn: 6.5,
     state: false,
     image:
@@ -42,7 +42,7 @@ const bondType: BondType[] = [
     name: "ATM No. 3",
     investAmount: "50000",
     interest: 12,
-    paid: "Paid by Annually",
+    paid: "Interest-Only Plan",
     investReturn: 12,
     state: false,
     image:
@@ -63,30 +63,17 @@ export default function USOverview() {
   };
 
   return (
-    <div id="container" className="container p-5">
+    <div id="container" className="container md:p-16 p-3">
       <h1 className="py-3 font-bold text-start md:text-4xl text-2xl">
         ATM: Antelope Texas Machine Bond
       </h1>
       <p>
         Explore the benefits of investing in ATM, a fund designed to provide
-        stable returns while supporting the growth and stability of Texas&rsquo;s
-        electricity infrastructure.
+        stable returns while supporting the growth and stability of
+        Texas&rsquo;s electricity infrastructure.
       </p>
-      <div className="grid md:grid-cols-5 grid-cols-1 gap-6 py-5">
-        <div className="md:col-span-4">
-          <div
-            className={`transition-opacity duration-300 ${
-              fadeIn ? "opacity-100" : "opacity-0"
-            }`}
-          >
-            <Image
-              className={"h-1/3 rounded-lg"}
-              src={bondType[selectedIndex].image} // Display the image corresponding to the selected card
-              alt="Revenue Forecast"
-            />
-          </div>
-        </div>
-        <div className="grid md:grid-rows-3 grid-cols-1 py-5">
+      <div className="grid md:grid-cols-3 grid-cols-1 gap-2 py-5">
+        <div className="grid col-span-3 md:grid-cols-3 grid-cols-1 md:gap-6 gap-1 py-5">
           {bondType.map((entry, index) => {
             return (
               <div key={index} className="py-4 ">
@@ -112,23 +99,17 @@ export default function USOverview() {
                     }
                     description={
                       <div className="flex-col">
-                        {/* <p className="text-start text-base tracking-normal">
-                          Investment Return:{" "}
-                          <span className="underline tracking-widest font-bold text-xl text-green">
-                            {entry.investReturn}%
-                          </span>
-                        </p> */}
                         <p className="text-start text-base tracking-normal underline capitalize underline-offset-2">
                           {entry.paid}
                         </p>
                         <p className="text-start text-base tracking-normal">
                           Annual Interest Rate:{" "}
-                          <span className="tracking-widest font-bold text-xl text-green">
-                            {entry.interest}%
+                          <span className="tracking-wide font-bold text-xl text-green">
+                            Up To {entry.interest}%
                           </span>
                         </p>
-                        <p className="text-start text-xs tracking-normal pt-2">
-                          Investment Period: 12 Month
+                        <p className="text-start text-xs tracking-normal pt-2 capitalize">
+                          Investment Period: up to 36 Month
                         </p>
                       </div>
                     }
@@ -138,18 +119,38 @@ export default function USOverview() {
             );
           })}
         </div>
-        <div className="md:col-span-4">
+        <div className="col-span-3">
           <Checkout index={selectedIndex} />
         </div>
-        <div className="md:col-span-2 md:col-start-4 md:text-end text-center">
+        <div className="col-span-3 md:text-end text-center">
           <p>If you are interested in this product, please contact us.</p>
         </div>
-        <div className="md:col-start-5 pb-10">
+        <div className="md:col-span-1 col-span-3 md:col-start-3 pb-10">
           <Link href="/contact-us" passHref>
             <Button type="primary" className="w-full" size="large">
               <ContactsOutlined /> Contact Us
             </Button>
           </Link>
+        </div>
+
+        <div className="col-span-3 justify-center">
+          <p>
+            <span className="text-2xl text-black font-bold tracking-wide"> Example Investment Breakdown:</span>
+            <br />
+            The table below illustrates an example of the monthly payments an
+            investor would receive, including both interest and principal.
+          </p>
+          <div
+            className={`transition-opacity duration-300 ${
+              fadeIn ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <Image
+              className={"h-1/3 rounded-lg md:max-h-[700px] max-h-[300px]"}
+              src={bondType[selectedIndex].image} // Display the image corresponding to the selected card
+              alt="Revenue Forecast"
+            />
+          </div>
         </div>
       </div>
     </div>
