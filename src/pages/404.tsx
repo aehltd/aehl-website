717d9297c4f4939cdaa82d16e0a5395b96fb3313
Junyu (Tim) Yao <197yjy@gmail.com>
2024-11-14 12:51:10 -0500

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,11 +2,14 @@ import Link from "next/link";;
 
 export default function NotFound() {
   return (
-    <div id="container" className="container">
-      <div className="row">
-        <h1>404</h1>
-        <p>Page not found</p>
-        <Link href="/">Go to Home</Link>
+    <div className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-base font-semibold text-indigo-600">404</p>
+        <h1 className="mt-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">Page not found</h1>
+        <p className="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">Sorry, we couldn’t find the page you’re looking for.</p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <a href="\" className="text-sm font-semibold text-gray-900">Go back home<span aria-hidden="true">&rarr;</span></a>
+        </div>
       </div>
     </div>
   );
