96d288c9d953a93860a80505d8c5ec81e19aa4f8
Junyu (Tim) Yao <197yjy@gmail.com>
2024-12-10 13:56:18 -0500

    Update btc-overview.tsx
    disable btc page

--- src/pages/btc/btc-overview.tsx
+++ src/pages/btc/btc-overview.tsx
@@ -1,14 +1,14 @@
-import BtcInfo from "../../components/btc/AehlBtcInfo"
+// import BtcInfo from "../../components/btc/AehlBtcInfo"
 // import AehlBtcBanner from "../../components/banner/AehlBtcBanner";
-import AehlBtcPromote from "../../components/btc/AehlBtcPromote";
-import AehlBtcFeature from "../../components/btc/AehlBtcFeature";
+// import AehlBtcPromote from "../../components/btc/AehlBtcPromote";
+// import AehlBtcFeature from "../../components/btc/AehlBtcFeature";
 // import AehlBtcStatistic from "../../components/btc/AehlBtcStatistic";
 
 
 export default function btcOverview() {
     return (
         <div id="container" className="container">
-            <div className="relative xl:w-[120vh] w-full">
+            {/* <div className="relative xl:w-[120vh] w-full">
                 <div className="bg-black w-full">
                     <AehlBtcPromote />
                 </div>
@@ -17,12 +17,13 @@ export default function btcOverview() {
             <div className="row">
                 <BtcInfo />
             </div>
-            {/* <div className="row">
+            <div className="row">
                 <AehlBtcStatistic />
-            </div> */}
+            </div>
             <div className="row">
                 <AehlBtcFeature />
-            </div>
+            </div> */}
+            <p>Page are not Found</p>
         </div>
     );
 }
