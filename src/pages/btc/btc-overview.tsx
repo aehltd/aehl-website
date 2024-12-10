9c4ae9f162a7b2cc9a7e5315ea325caa681660f2
Junyu (Tim) Yao <197yjy@gmail.com>
2024-12-10 13:50:26 -0500

    Update btc-overview.tsx
    bug fixing

--- src/pages/btc/btc-overview.tsx
+++ src/pages/btc/btc-overview.tsx
@@ -1,8 +1,8 @@
 import BtcInfo from "../../components/btc/AehlBtcInfo"
-import AehlBtcBanner from "../../components/banner/AehlBtcBanner";
+// import AehlBtcBanner from "../../components/banner/AehlBtcBanner";
 import AehlBtcPromote from "../../components/btc/AehlBtcPromote";
 import AehlBtcFeature from "../../components/btc/AehlBtcFeature";
-import AehlBtcStatistic from "../../components/btc/AehlBtcStatistic";
+// import AehlBtcStatistic from "../../components/btc/AehlBtcStatistic";
 
 
 export default function btcOverview() {
@@ -12,12 +12,6 @@ export default function btcOverview() {
                 <div className="bg-black w-full">
                     <AehlBtcPromote />
                 </div>
-                {/* <div className="relative z-0">
-                    <AehlBtcBanner />
-                </div>
-                <div className=" absolute inset-0 m-auto top-16 right-6 bg-transparent z-10 w-5/12">
-                    <AehlBtcPromote />
-                </div> */}
             </div>
 
             <div className="row">
