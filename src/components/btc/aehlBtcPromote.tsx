c9c4d73d8c9f6fef961733b2c820afdf362036fc
Junyu (Tim) Yao <197yjy@gmail.com>
2024-12-11 11:50:52 -0500

    btc promote test

--- src/components/btc/aehlBtcPromote.tsx
+++ src/components/btc/aehlBtcPromote.tsx
@@ -1,62 +1,64 @@
-import React from 'react';
+import React from "react";
 import { Carousel } from 'antd';
 
 import AehlBtcBanner from "../banner/AehlBtcBanner";
-// 
-const videoInfo = [
-    {
-        title: "btcShort video image 1",
-        imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293501/2_efdt2j.png"
-    },
-    {
-        title: "btcShort video image 2",
-        imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293501/3_ojin7l.png"
-    },
-    {
-        title: "btcShort video image 3",
-        imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293501/4_wv2cq9.png"
-    },
-    {
-        title: "btcShort video image 4",
-        imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293501/5_uetxi9.png"
-    },
-    {
-        title: "btcShort video image 5",
-        imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293501/6_cccyas.png"
-    },
-    {
-        title: "btcShort video image 6",
-        imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293501/7_pfyr2z.png"
-    },
-    {
-        title: "btcShort video image 7",
-        imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293502/8_vhvr7j.png"
-    },
-    {
-        title: "btcShort video image 8",
-        imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293502/9_rgpyry.png"
-    },
-    {
-        title: "btcShort video image 9",
-        imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293502/10_jedimm.png"
-    },
-    {
-        title: "btcShort video image 10",
-        imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293502/11_ktqgpt.png"
-    },
-    {
-        title: "btcShort video image 11",
-        imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293961/12_xxdgwd.png"
-    },
-    {
-        title: "btcShort video image 12",
-        imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293961/13_y0n85v.png"
-    }
-];
+
 
 export default function AehlBtcPromote() {
+
+    const videoInfo = [
+        {
+            title: "btcShort video image 1",
+            imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293501/2_efdt2j.png"
+        },
+        {
+            title: "btcShort video image 2",
+            imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293501/3_ojin7l.png"
+        },
+        {
+            title: "btcShort video image 3",
+            imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293501/4_wv2cq9.png"
+        },
+        {
+            title: "btcShort video image 4",
+            imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293501/5_uetxi9.png"
+        },
+        {
+            title: "btcShort video image 5",
+            imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293501/6_cccyas.png"
+        },
+        {
+            title: "btcShort video image 6",
+            imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293501/7_pfyr2z.png"
+        },
+        {
+            title: "btcShort video image 7",
+            imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293502/8_vhvr7j.png"
+        },
+        {
+            title: "btcShort video image 8",
+            imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293502/9_rgpyry.png"
+        },
+        {
+            title: "btcShort video image 9",
+            imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293502/10_jedimm.png"
+        },
+        {
+            title: "btcShort video image 10",
+            imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293502/11_ktqgpt.png"
+        },
+        {
+            title: "btcShort video image 11",
+            imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293961/12_xxdgwd.png"
+        },
+        {
+            title: "btcShort video image 12",
+            imageSrc: "https://res.cloudinary.com/aehl/image/upload/v1732293961/13_y0n85v.png"
+        }
+    ];
+
     return (
-        <div className="">
+        <div>
             <Carousel arrows autoplay infinite={true} autoplaySpeed={1350} effect='scrollx'>
 
                 {/* templete */}
