f4760f2c653fd0772d52cdde1a62ec9b79152377
Junyu (Tim) Yao <197yjy@gmail.com>
2025-01-22 09:54:38 -0500

    Update btc-overview.tsx

--- src/pages/btc/btc-overview.tsx
+++ src/pages/btc/btc-overview.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 
-// import BtcInfo from "../../components/btc/AehlBtcInfo"
-// import AehlBtcFeature from "../../components/btc/AehlBtcFeature"
-// import AehlBtcTop from "../../components/btc/AehlBtcTop"
+import BtcInfo from "../../components/btc/AehlBtcInfo"
+import AehlBtcFeature from "../../components/btc/AehlBtcFeature"
+import AehlBtcTop from "../../components/btc/AehlBtcTop"
 
 
 export default function btcOverview() {
     return (
         <div id="container" className="container">
-            {/* <div className="relative xl:w-[120vh] w-full">
+            <div className="relative xl:w-[120vh] w-full">
                 <div className="bg-black w-full">
                     <AehlBtcTop />
                 </div>
@@ -17,13 +17,13 @@ export default function btcOverview() {
 
             <div className="row">
                 <BtcInfo />
-            </div> */}
+            </div>
             {/* <div className="row">
                 <AehlBtcStatistic />
             </div> */}
-            {/* <div className="row">
+            <div className="row">
                 <AehlBtcFeature />
-            </div> */}
+            </div>
             <p>Page are not Found</p>
         </div>
     );
